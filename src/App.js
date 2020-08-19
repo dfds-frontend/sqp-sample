041a131c70cb90648a33203ada9e704d4c200d93
Ionut Paraschiv <iopar@dfds.com>
2020-08-19 17:39:28 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import qs from 'qs';
+const { createHash } = require("crypto"); //this is a node module not npm
+
+
+// Helper methods
+const generateRandomString = (length) =>{
+  let text = "";
+  const whitelist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+  for (var i = 0; i < length; i++) {
+      text += whitelist.charAt(Math.floor(Math.random() * whitelist.length));
+  }
+  return text;
+}
+
+const generateChallenge = (code_verifier) => {
+  const hash = createHash("sha256")
+    .update(code_verifier)
+    .digest("base64");
+  return base64UrlEncode(hash);
+}
+
+const base64UrlEncode = (base64) => {
+  return base64
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+}
+
+const getUrlParam = (param) =>{
+  return new URLSearchParams(window.location.search).get(param);
+}
+
+const exchangeCode = (code )=>{
+  const headers = {
+    'Accept' : 'application/json', 
+    'Content-Type': 'application/x-www-form-urlencoded',
+  }
+  const reqBody = {
+    grant_type: 'authorization_code',
+    client_id,
+    redirect_uri,
+    code,
+    code_verifier
+  }
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {...headers},
+    body: qs.stringify(reqBody),
+    redirect: 'follow'
+  };
+
+  return fetch(`${oktaDomain}/oauth2/default/v1/token`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+
+//Okta configuration 
+const oktaDomain = 'https://dev-accounts.dfds.com' // your okta issuer, most likely accounts.dfds.com and changed accordingly per environment
+const client_id = '0oah1b1mpv3cww8pe0x6' // okta client id
+const redirect_uri = 'https://localhost:3000' // whitelisted redirect uri
+
+//pkce code challange - Change this to fit your needs
+const code_verifier = localStorage.getItem('codeVerifier') ? localStorage.getItem('codeVerifier') : generateRandomString(43)
+localStorage.setItem("codeVerifier", code_verifier)
+const challange = generateChallenge(code_verifier)
 
 function App() {
+  if(getUrlParam('code')){
+    exchangeCode(getUrlParam('code'))
+      .then( res =>{
+        localStorage.removeItem('codeVerifier') // remove code verifier since it's already been used. new one should be created
+        window.history.replaceState(null, null, window.location.pathname); // clean up the url
+      })
+      .catch( err => {
+        console.log(err)
+      })
+  }
+const url = `${oktaDomain}/oauth2/default/v1/authorize?
+&response_type=code
+&scope=openid
+&redirect_uri=${redirect_uri}
+&state=state-${challange}
+&code_challenge_method=S256
+&client_id=${client_id}
+&code_challenge=${challange}`
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a className="link" href={url}>go to ad for login</a>
     </div>
   );
 }
